ac167913c2e5d4cd4715bb6634e4c4f8c8c0d3e2
andrey <sobolev@bio.lmu.de>
2015-12-08 11:38:08 +0100

    add proper annotation to phototransistor stetch

--- phototransistor/phototransistor.ino
+++ phototransistor/phototransistor.ino
@@ -1,16 +1,8 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  Simple sketch to test the phototransistor.
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
+  Prints to Serial the input from phototransistor with 
+  a delay of half a second.
  */
 
 int val = 0;
